53d5d555fc7c8c8d18af139d3a04b7c699831fcd
hubteas <tykim888@naver.com>
2024-09-12 18:27:05 +0900

    20240912

--- workspace_html/task01.js
+++ workspace_html/task01.js
@@ -1,14 +1,30 @@
 let insertBtn = document.getElementById('insertBtn');
-let insertTask = document.getElementById('checkbox');
 let taskContainer = document.getElementById('task-container');
+let todoList = document.getElementById('todolist');
 
 console.log(insertBtn);
 
 insertBtn.addEventListener('click', ()=>{
-  let innerTask = prompt("할일을 입력하세요");
-  insertTask.innerText = innerTask;
-  console.log(insertTask.innerText)
-  if(insertTask.innerText!=''){
-    taskContainer.setAttribute('display', 'block');
-  }
+  let toDo = prompt("할일을 입력하세요");
+  console.log(toDo);
+
+  if(toDo){
+    taskContainer.style.display = 'block';
+    const newLi = document.createElement('li');
+		const newCheckbox = document.createElement('input');
+    newCheckbox.type = "checkbox";
+		const newBtn = document.createElement('button');
+    newBtn.innerText = '';
+      
+		newLi.appendChild(newCheckbox); // li안에 checkbox 담기
+		newLi.appendChild(newBtn); // li안에 button 담기
+		console.log(newLi);
+    newCheckbox.textContent = toDo;
+
+    todoList.appendChild(newLi);
+    console.log(todoList);
+
+
+  };
+  
 })
