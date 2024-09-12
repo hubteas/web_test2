4bf3a7c4d067bdbfd534289ad20b63e5a8179fe5
hubteas <tykim888@naver.com>
2024-09-12 21:59:38 +0900

    20240912 그대로

--- workspace_html/task01.js
+++ workspace_html/task01.js
@@ -8,16 +8,17 @@ insertBtn.addEventListener('click', ()=>{
   let toDo = prompt("할일을 입력하세요");
   console.log(toDo);
 
-  if(toDo){
+  if(toDo!=''){
     taskContainer.style.display = 'block';
     const newLi = document.createElement('li');
 		const newCheckbox = document.createElement('input');
     newCheckbox.type = "checkbox";
 		const newBtn = document.createElement('button');
-    newBtn.innerText = '';
+    newBtn.innerText = '삭제';
       
 		newLi.appendChild(newCheckbox); // li안에 checkbox 담기
 		newLi.appendChild(newBtn); // li안에 button 담기
+
 		console.log(newLi);
     newCheckbox.textContent = toDo;
 
